b5f4ad0b355f72fe26b9a28f3e0f32df09351776
kaancankat <kaancankat1@gmail.com>
2022-10-08 15:56:32 +0300

    we
    ew

---
+++ javaproje.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class PratikA {
+    public static void main(String[] args) {
+       /* int i, sayi;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz: ");
+        sayi = inp.nextInt();
+        for (i = 0; i <= sayi; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+        int i = 0, l;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("sayı giriniz: ");
+        l = inp.nextInt();
+        while (i <= l) {
+            i++;
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+        int k;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Sayı giriniz: ");
+        k = inp.nextInt();
+        for (int i = 1; i <= k; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                System.out.println(i);
+            }
+        }*/
+
+        int sayac=0,m,sum=0,sayilar=0,ort = 0;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("bir sayı giriniz: ");
+        m = inp.nextInt();
+        while (sayac<=m){
+            sayac++;
+            if (sayac % 3 == 0 && sayac % 4 == 0){
+                System.out.println(sayac);
+                sayilar++;
+                sum+=sayac;
+                ort=sum/sayilar; 
+            }
+        }System.out.println("sayilarin ort ="+ort);
+    }
+}
+
+
